5e4c04ee70308ba669abcbdd8db095b496d54747
Anna Calle <annagcalle@bren.ucsb.edu>
2020-03-30 23:06:25 -0700

    Updated about page

--- maricultura-app/app.R
+++ maricultura-app/app.R
@@ -63,32 +63,28 @@ ui <- fluidPage(
              # Do not need title for Navigation Bar
              # First tab
              tabPanel(div(icon("info-circle"),"About"),
-                      h1("The Project",
-                         class = "font-weight-light text-white",
-                         class = "bg-primary text-center py-5 mb-4"),
-                      h1("Meet the Creators",
-                         class = "font-weight-light text-white",
-                         class = "bg-primary text-center py-5 mb-4"),
-                      p(
-                        wellPanel(style = "padding = 15",
-                                   img(src = "maricultura-team.jpg",
-                                       height = 400,
-                                       width = 400,
-                                       align = "middle")
-                                   
-                        ),
-                        
-                        
-                        
-                        
-                        "MARICULTURA is a team of 5 bright graduate students studying at the Bren School of Environmental Science & Management at the University of California Santa Barbara.  A partnership with the World Wildlife Fund has enabled the team to shape marine aquaculture planning off the coast of Brazil...... 
-                        
-                        
-                        Visit our",
-                        a( href = "https://maricultura.weebly.com", "website"),
-                        "for more information."),
-                      div(class = "container")
-             ),
+                      HTML('<div class="jumbotron" style="height: 75vh; display: flex; justify-content: center; align-items: center; background: linear-gradient( rgba(197,219,235, 0.65), rgba(197,219,235, 0.8)),
+	url(https://images.unsplash.com/photo-1518837695005-2083093ee35b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80); background-size: cover; background-position: center top; letter-spacing: 0.7em;">
+                           <h1 class="display-3" style="text-align: center; color: white; font-size: 5em; font-weight: 550;">SHAPING &nbsp;MARICULTURE &nbsp;IN &nbsp;BRAZIL</h1>
+                           </div>' ),
+                      HTML('<div style="height: 75vh; display: table;" class="jumbotron">
+                      <div style="display: table-cell; vertical-align: middle;">
+  <h1 class="display-3" >The Project</h1>
+  <p class="lead">The <a href=https://www.bren.ucsb.edu target="_blank">Bren School</a> at UC Santa Barbara is a graduate-level environmental science and management program whose students fulfill a 12 month Group Project to solve an environmental problem faced by a real-world client, in our case, the World Wildlife Fund. We explored the spatial feasibility of offshore mariculture in Brazil’s Exclusive Economic Zone, and created a web-based tool to predict yields and profitability for offshore mariculture of various finfish. This app is the interactive component of our overall thesis project.</p>
+  </div>
+</div>'
+                      ),
+                      HTML('<div style="height: 75vh; overflow: auto;  display: table;" class="jumbotron">
+                        <div style="display: table-cell; vertical-align: middle;">
+  <h1 class="display-3"> Meet the Creators</h1>
+  <img style="width: 30%; left-padding: 20px; bottom-padding: 20px; display: block; float:right; vertical-align: middle" src="maricultura-team.jpg" alt="Team image">
+  <p class="lead">Our team consists of 5 master’s students at the Bren School of Environmental Science & Management at the University of California, Santa Barbara: Anna Calle, Caio Vianna, Eva Marrero, Kirby Barttlet and Sandra Fogg. We came together as a group with a common interest in marine conservation and ocean sustainability. Our collaborative thesis is in partnership with the World Wildlife Fund in Brazil.</p>
+  <p class="lead">For more information visit our website.</p>
+  <p class="lead">
+    <a style="vertical-align: bottom;" class="btn btn-primary btn-lg" href=""https://maricultura.weebly.com"" role="button">Learn more</a>
+  </p>
+  </div>
+</div>')),
              
              
              # Second tab
