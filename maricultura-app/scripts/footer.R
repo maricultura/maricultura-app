17946efc461738945838546ed178a039251ef042
Anna Calle <annagcalle@bren.ucsb.edu>
2020-10-12 19:29:12 -0500

    Fixed link

--- maricultura-app/scripts/footer.R
+++ maricultura-app/scripts/footer.R
@@ -5,5 +5,5 @@ footer <- div( style = "background-color: #c5dbeb; padding: 15px; text-align: ce
                            img(src = "https://rstudio.com/wp-content/uploads/2018/10/RStudio-Logo-Flat.png", height = "30px"),
                            ".",
                            br(),
-                           "R version 3.6.1 (2019-07-05). Code on  ", tags$a(href ="https://github.com/annagaby/tree-monitoring", target="_blank", icon("github"),"GitHub."))
+                           "R version 3.6.1 (2019-07-05). Code on  ", tags$a(href ="https://github.com/maricultura/maricultura-app", target="_blank", icon("github"),"GitHub."))
 )
